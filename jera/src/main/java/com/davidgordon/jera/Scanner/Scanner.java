7fab67ed422550207c478608ea49091b1ad28a33
tehtko <moderndsnsam@gmail.com>
2023-09-06 16:49:17 -0400

    Begin implementing our scanner (lexer)

---
+++ jera/src/main/java/com/davidgordon/jera/Scanner/Scanner.java
@@ -0,0 +1,107 @@
+package com.davidgordon.jera.Scanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.davidgordon.jera.Main;
+import com.davidgordon.jera.Token.Token;
+import com.davidgordon.jera.Token.TokenType;
+
+public class Scanner {
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+
+  private final String source;
+  private final List<Token> tokens = new ArrayList<Token>();
+
+  public Scanner(String source) {
+    this.source = source;
+  }
+
+  public List<Token> scanTokens() {
+    while(!isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private void scanToken() {
+    char c = nextChar();
+
+    switch(c) {
+      case '(': addToken(TokenType.LEFT_PAREN); break;
+      case ')': addToken(TokenType.RIGHT_PAREN); break;
+      case '{': addToken(TokenType.LEFT_BRACE); break;
+      case '}': addToken(TokenType.RIGHT_BRACE); break;
+      case ',': addToken(TokenType.COMMA); break;
+      case '.': addToken(TokenType.DOT); break;
+      case '-': addToken(TokenType.MINUS); break;
+      case '+': addToken(TokenType.PLUS); break;
+      case ';': addToken(TokenType.SEMICOLON); break;
+      case '*': addToken(TokenType.MULTIPLY); break;
+      case '/': addToken(TokenType.DIVIDE); break;
+      case '#': 
+        while(peek() != '\n' && !isAtEnd()) nextChar();
+        break;
+      case '!':
+        addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace.
+        break;
+
+      case '\n':
+        line++;
+        break;
+      default: 
+        Main.error(line, "Unexpected character.");
+        break;
+    }
+  }
+
+  private char nextChar() {
+    return source.charAt(current++);
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+}
