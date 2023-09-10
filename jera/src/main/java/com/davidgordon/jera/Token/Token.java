325bacbe44276800990ce69ca1519562a92dac36
davidgordon12 <davidgordon12@live.ca>
2023-09-10 13:02:23 -0400

    Add a list of reserved keywords

--- jera/src/main/java/com/davidgordon/jera/Token/Token.java
+++ jera/src/main/java/com/davidgordon/jera/Token/Token.java
@@ -1,5 +1,8 @@
 package com.davidgordon.jera.Token;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Token {
   final TokenType tokenType;
   final String lexeme;
@@ -16,4 +19,26 @@ public class Token {
   public String toString() {
     return tokenType + " " + lexeme + " " + literal;
   }
+
+  public static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<String, TokenType>();
+    keywords.put("and",    TokenType.AND);
+    keywords.put("class",  TokenType.CLASS);
+    keywords.put("else",   TokenType.ELSE);
+    keywords.put("false",  TokenType.FALSE);
+    keywords.put("for",    TokenType.FOR);
+    keywords.put("def",    TokenType.DEF);
+    keywords.put("if",     TokenType.IF);
+    keywords.put("none",   TokenType.NONE);
+    keywords.put("or",     TokenType.OR);
+    keywords.put("print",  TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super",  TokenType.SUPER);
+    keywords.put("this",   TokenType.THIS);
+    keywords.put("true",   TokenType.TRUE);
+    keywords.put("var",    TokenType.VAR);
+    keywords.put("while",  TokenType.WHILE);
+  }    
 }
